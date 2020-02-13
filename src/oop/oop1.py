7c00434efe04db9b87e7ae95abcf23406097e2c3
ropeks <ropeksmilioner@gmail.com>
2020-02-13 13:42:14 +0100

    do oop1 excercise

--- src/oop/oop1.py
+++ src/oop/oop1.py
@@ -17,3 +17,28 @@
 #     pass
 #
 # Put a comment noting which class is the base class
+
+class Vehicle: # <--- BASE CLASS
+    pass
+
+
+
+class GroundVehicle(Vehicle):
+    pass
+
+class Car(GroundVehicle):
+    pass
+
+class Motorcycle(GroundVehicle):
+    pass
+
+
+
+class FlightVehicle(Vehicle):
+    pass
+
+class Airplane(FlightVehicle):
+    pass
+
+class Starship(FlightVehicle):
+    pass
